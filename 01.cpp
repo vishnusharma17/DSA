98eebf95340e4706e1871f8e05a1ea7b4a582d8c
vishnu sharma <159338324+vishnusharma17@users.noreply.github.com>
2024-05-09 09:18:41 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,66 @@
+//array
+
+//addition of some elements using an array 
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int arr[5] = {1,2,3,4,5};
+// int sum  = 0 ;
+// for(int i = 0 ; i < 5; i ++ ){
+//     sum = sum + arr[i];
+// }
+//     cout<<"the sum of array elements :"<<sum<<endl;
+//     return 0;
+// }
+
+//find max number using array 
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int arr[5] = {1,2,3,4,5};
+// int max = arr[0];
+// for(int i = 0; i < 5; i++){
+//     if(arr[i] > max ){
+//         max = arr[i];
+//     }
+// }
+// cout<<"the maximum number is :"<<max<<endl;
+//     return 0;
+// }
+
+//find even number using array 
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     int arr[6] = { 11,20,41,5,6,70};
+//     int sum = 0;
+//     for(int i = 0; i < 6; i++){
+//         if(arr[i]%2==0){
+//             sum = sum + 1;
+//         }
+//     }
+//     cout <<"the even number is :"<<sum<<endl;
+// }
+
+//find pairs of two variables 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[5] = {3,2,4,1,5};
+//     int sum = 5;
+//     int count = 0;
+//     for(int i = 0 ; i< 5; i++){
+//         for(int j = i+1 ; j < 5; j ++){
+
+//         if(arr[i]+ arr[j]==sum){
+//             count = count+1;
+//         }
+//         }
+//     }
+//     cout<<"the sum of pairs of variables :"<<count<<endl;
+// }
+
+//
+
+
